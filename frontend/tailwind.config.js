2bb21e466f9d47301556796e93723e82b8c01315
Trevor McHaffie <mr1018@gmail.com>
2025-03-19 15:57:26 -0400

    Tailwind fixed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,19 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: '#3490dc',
-        secondary: '#ffed4a',
-        danger: '#e3342f',
-        success: '#38c172',
-        warning: '#f6993f',
+        primary: "#3490dc",
+        secondary: "#ffed4a",
+        danger: "#e3342f",
+        success: "#38c172",
+        warning: "#f6993f",
       },
     },
   },
   plugins: [],
-}
+};
