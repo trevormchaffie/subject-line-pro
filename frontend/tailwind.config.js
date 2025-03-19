53b8b1a70a4c72443d4cb6116ebf630e42159b2e
Trevor McHaffie <mr1018@gmail.com>
2025-03-19 14:05:13 -0400

    Set up Tailwind CSS

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3490dc',
+        secondary: '#ffed4a',
+        danger: '#e3342f',
+        success: '#38c172',
+        warning: '#f6993f',
+      },
+    },
+  },
+  plugins: [],
+}
